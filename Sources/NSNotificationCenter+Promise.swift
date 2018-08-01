10e4c56061e1b593e5bbbac432875e1a58508c8c
Maksim Orlovich <maksimorlovich@users.noreply.github.com>
2018-08-01 13:28:25 -0500

    test

--- Sources/NSNotificationCenter+Promise.swift
+++ Sources/NSNotificationCenter+Promise.swift
@@ -25,7 +25,7 @@ extension NotificationCenter {
       #if !os(Linux)
         let id = addObserver(forName: name, object: object, queue: nil, using: fulfill)
       #else
-        #if swift(>=4.0.1)
+        #if swift(>=4.1)
           let id = addObserver(forName: name, object: object, queue: nil, using: fulfill)
         #else
           let id = addObserver(forName: name, object: object, queue: nil, usingBlock: fulfill)
